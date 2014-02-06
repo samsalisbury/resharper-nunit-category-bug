6ba71b701285e7f2b1d502bce96f5342bacf8bb7
Sam Salisbury <samsalisbury@gmail.com>
2014-02-06 12:21:27 +0000

    Initial Commit

---
+++ ResharperTestRunnerCategoryBug/MyTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace ResharperTestRunnerCategoryBug
+{
+	[TestFixture(true, Category = "Cat1")]
+	[TestFixture(false, Category = "Cat2")]
+	public class MyTestFixture
+	{
+		private readonly bool _trueOrFalse;
+
+		public MyTestFixture(bool trueOrFalse)
+		{
+			_trueOrFalse = trueOrFalse;
+		}
+
+		[Test]
+		public void MyTest()
+		{
+			Assert.True(_trueOrFalse);
+		}
+	}
+}
